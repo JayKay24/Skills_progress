a0188fd068c3e76358c8bca0e69e8d0c51dd8586
James Kinyua <jameskinyua590@gmail.com>
2017-04-07 14:05:14 +0300

    Add a function main to steer the overall project

--- skills_progress.py
+++ skills_progress.py
@@ -17,3 +17,19 @@ def display_greeting():
     print("\tusing the learning map.")
     print("\n{:*^80s}\n".format("*"))
 
+def main():
+    # Display the program greeting.
+    display_greeting()
+
+    # Ask if the user wants to continue.
+    while True:
+        response = input("\nContinue? (y/n): ")
+        if response in "yYnN":
+            break
+        print("Invalid response")
+    if response.lower() == "n":
+        print("\nGoodbye")
+        break
+
+if __name__ == "__main__":
+    main()
