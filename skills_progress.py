2a458add5b79b4a8a9d9e73f1aec707851de95f7
James Kinyua <jameskinyua590@gmail.com>
2017-04-07 14:01:26 +0300

    Add a function display_greeting

---
+++ skills_progress.py
@@ -0,0 +1,19 @@
+"""
+This program helps boot camp participants track their progress
+using the learning map.
+"""
+
+import sys
+
+def display_greeting():
+    """
+    Formats and prints the program greeting.
+    """
+    # Init.
+    title = " Skills progress "
+
+    print("\n{:*^80s}\n".format(title.title()))
+    print("\tThis program helps boot camp participants track their progress")
+    print("\tusing the learning map.")
+    print("\n{:*^80s}\n".format("*"))
+
